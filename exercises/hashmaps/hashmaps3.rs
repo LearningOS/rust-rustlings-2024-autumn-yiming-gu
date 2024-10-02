bb31eeb94501389e9475a53a6e2279539ba567ec
yiming-gu <1642802130@qq.com>
2024-10-02 18:02:44 +0800

    pass quiz2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,42 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let old_team_1 = scores.get(&team_1_name);
+        match old_team_1 {
+            None => {
+                let team = Team {
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                };
+                scores.insert(team_1_name, team);
+            },
+            Some(team) => {
+                let new_team = Team {
+                    goals_scored: team.goals_scored + team_1_score,
+                    goals_conceded: team.goals_conceded + team_2_score,
+                };
+                scores.insert(team_1_name, new_team);
+            }
+        }
+
+        let old_team_2 = scores.get(&team_2_name);
+        match old_team_2 {
+            None => {
+                let team = Team {
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                };
+                scores.insert(team_2_name, team);
+            },
+            Some(team) => {
+                let new_team = Team {
+                    goals_scored: team.goals_scored + team_2_score,
+                    goals_conceded: team.goals_conceded + team_1_score,
+                };
+                scores.insert(team_2_name, new_team);
+            }
+        }
     }
     scores
 }
